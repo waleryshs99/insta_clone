cab3c7a7dc57cdb1660926cb4e27e82ef7972c8f
Mikołaj <m.basinski@outlook.com>
2017-10-13 12:31:31 +0200

    add models

--- clone_a_gram/models.py
+++ clone_a_gram/models.py
@@ -1,3 +1,22 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    path = models.TextField()
+    creation = models.DateField(auto_now_add=datetime.datetime.now())
+    added_by = models.ForeignKey(User)
+
+
+class Comment(models.Model):
+    body = models.CharField(max_length=256)
+    author = models.ForeignKey(User)
+    date = models.DateField(auto_now_add=datetime.datetime.now())
+    commented_photo = models.ForeignKey(Photo)
+
+
+class Like(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(User)
